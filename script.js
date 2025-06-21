117ab8e90f72500d7a0d8525effab470b0d5d43f
Panashe Chipendo <106480889+panashechipendo@users.noreply.github.com>
2025-06-21 17:30:47 +0200

    Add calculation logic for two numbers

--- script.js
+++ script.js
@@ -2,35 +2,69 @@ const numbers = document.querySelectorAll(".digit");
 const symbols = document.querySelectorAll(".symbol");
 const equals = document.querySelector(".equals");
 const display = document.querySelector(".input-container");
-const text = document.createElement("h3");
+const text = document.createElement("h2");
+display.appendChild(text);
+text.textContent = "";
 
 const zeroToNine = Array.from(numbers);
 const symbolArray = Array.from(symbols);
 
-function appendToInput(event) {
-  let item = event.target.value;
-  text.textContent += item;
+let operand1 = [];
+let operand2 = [];
+let operator = [];
+
+function add(a, b) {
+  return Number(a) + Number(b);
 }
 
-let exp = [];
-function add(...a) {
-  a.reduce((acc, curr) => acc + curr);
+function subtract(a, b) {
+  return Number(a) - Number(b);
 }
 
-function subtract(...a) {
-  a.reduce((acc, curr) => acc + curr);
+function multiply(a, b) {
+  return Number(a) * Number(b);
 }
 
-function multiply(...a) {
-  a.reduce((acc, curr) => acc + curr);
+function divide(a, b) {
+  return Number(a) / Number(b);
 }
-
-function divide(...a) {
-  a.reduce((acc, curr) => acc * curr);
-}
-
-function evaluate(expression) {}
 
 zeroToNine.forEach((number) => {
-  number.addEventListener("click", appendToInput);
+  number.addEventListener("click", (e) => {
+    let item = e.target.value;
+    text.textContent += item;
+    console.log(item);
+  });
 });
+
+symbols.forEach((symbol) => {
+  symbol.addEventListener("click", (e) => {
+    let item = e.target.value;
+    console.log(typeof item);
+    text.textContent += item;
+    operator.push(item);
+  });
+});
+
+function evaluate(exp) {
+  let result;
+  for (let i = 0; i < exp.length; i++) {
+    if (exp[i] == "+") {
+      result = add(exp.slice(0, i).join(""), exp.slice(i + 1).join(""));
+      text.textContent = result;
+    } else if (exp[i] == "-") {
+      result = subtract(exp.slice(0, i).join(""), exp.slice(i + 1).join(""));
+      text.textContent = result;
+    } else if (exp[i] == "*") {
+      result = multiply(exp.slice(0, i).join(""), exp.slice(i + 1).join(""));
+      text.textContent = result;
+    } else if (exp[i] == "/") {
+      result = divide(exp.slice(0, i).join(""), exp.slice(i + 1).join(""));
+      text.textContent = result;
+    }
+  }
+
+  return result;
+}
+
+console.log(evaluate(["1", "2", "-", "1", "2"]));
